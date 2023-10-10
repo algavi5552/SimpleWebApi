79e547022bba751d59fe198df2a5bad35c73dc01
Pavel <algave28@gmail.com>
2023-10-10 17:22:25 +0300

    try to add EF

---
+++ Simple_REST_API_App/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Simple_REST_API_App
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = ""; // имя пользователя
+        public int Age { get; set; } // возраст пользователя
+    }
+}
